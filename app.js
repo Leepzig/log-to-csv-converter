8c7fb59f366ea2419cb1e63bcf0a73fecce245a3
andrewsimmons <neon.duncan@gmail.com>
2022-01-12 17:17:08 -0600

    sucessfully read a list of json files and wrote them into one file, but as json

--- app.js
+++ app.js
@@ -14,24 +14,29 @@ const path = require('path');
 //     });
 // }
 const readJson = filePath => {
-    let data = fs.createReadStream(filePath, {encoding: 'utf8'})
-    let student = JSON.parse(data);
-    console.log(student);
+    let rs = fs.createReadStream(filePath)
+    return rs
+    // rs.on('data', (dataChunk) => {
+    //     let student = JSON.parse(dataChunk)
+    //     console.log(student)
+    // })
 }
 
 const writeJson = (data, file) => {
     let jsonData = JSON.stringify(data, null, 2)
-    fs.writeFile(file, jsonData, (err) => {
+    fs.writeFile(file, jsonData, {flag:'a+'}, (err) => {
         if (err) throw err;
         console.log('Data written to file');
     });
 }
 
 
-// fs.readdirSync('students').forEach( fileName => {
-//     let dirPath = path.join(__dirname, `/students/${fileName}`)
-//     let student = readJson(dirPath)
-//     console.log(student)
-// })
-const dirPath = path.join(__dirname, '/students/student.json')
-readJson(dirPath)
+fs.readdirSync('students').forEach( fileName => {
+    let dirPath = path.join(__dirname, `/students/${fileName}`)
+    let student = readJson(dirPath)
+    student.on('data', (dataChunk) => {
+        writeJson(JSON.parse(dataChunk), 'data.json')
+    })
+})
+// const dirPath = path.join(__dirname, '/students/student.json')
+// readJson(dirPath)
