95e57f0841b1b9a8673ac0c98216f329f271d9f8
andrewsimmons <neon.duncan@gmail.com>
2022-01-13 16:06:11 -0600

    sucessfully wrote to file using CSV formatting, now to just make it using one command

--- NOTES.md
+++ NOTES.md
@@ -2,3 +2,4 @@ we need to read and write the files using streams
 when writing json, it needs to be formatted into an array, or something, depends
 now how to format json into a csv
 
+filter by .gz
--- flowToCsv.js
+++ flowToCsv.js
@@ -1,26 +1,18 @@
 
 const fs = require('fs')
-const { dirname } = require('path')
 const path =require('path')
+const { stringify } = require('querystring')
+var zlib = require('zlib')
+var gunzip = zlib.createGunzip()
+const readline = require('readline');
 
-//fix the array
-const dir = fs.readdirSync('AWSLogs')[1]
-const dirAWS = path.join(__dirname,'AWSLogs', dir)
-const dirSub = path.join(dirAWS, 'vpcflowlogs')
 
 //returns a boolean for if file is a directory
 const isDir = (filePath) => {
     const result = fs.statSync(filePath, (er, stats) => {if (er) throw er})
     return !result.isFile()
 }
-// console.log(isDir(dir))
-// console.log(isDir(dirAWS))
 
-// fs.readdirSync(dir).forEach(fileName => {
-//     console.log(fileName)
-//     const dirPath = path.join(__dirname, fileName)
-//     // console.log(dirPath)
-// })
 // using recursion, drills down to the files
 const drillDownToFiles = (rootPath, folder) => {
     const dirTitle = fs.readdirSync(folder)[1]
@@ -30,14 +22,64 @@ const drillDownToFiles = (rootPath, folder) => {
     if (isDir(dirPath)) {
         drillDownToFiles(dirPath, newFolderPath)
    } else {
-       console.log("was not folder")
+       console.log("was not folder", dirPath)
     fs.readdirSync(folder).forEach(fileName => {
-   console.log("loop:", fileName)})
+        console.log("File:", fileName)
+        //unzip(dirpath/filename)
+        })
     }
 }
-const awsLogs = "AWSLogs"
-const rootPath = path.join(__dirname, awsLogs)
-drillDownToFiles(rootPath, awsLogs)
+//Used to execute Looping through files:
+// const awsLogs = "AWSLogs"
+// const rootPath = path.join(__dirname, awsLogs)
+// drillDownToFiles(rootPath, awsLogs)
+
+const logExample = "AWSLogs/607487649814/vpcflowlogs/us-east-2/2022/01/13/05/607487649814_vpcflowlogs_us-east-2_fl-007edc593c7c0b891_20220113T0525Z_31dbb2ff.log.gz"
+
+const test = fs.readFileSync(logExample)
+
+// console.log(test.toString().replaceAll(" ", ","))
+const writeToCsv = filePath => {
+    let lineReader = readline.createInterface({
+        input: fs.createReadStream(filePath).pipe(zlib.createGunzip())
+    });
+    
+    lineReader.on('line', (line) => {
+        // fs.writeFileSync("logs.csv", line.replaceAll(" ", ","), {flags:'a'}, (err) => {
+            console.log(line.replaceAll(" ", ","))
+            // if (err) throw err
+        // });
+    });
+}
+
+writeToCsv(logExample)
+
+
+
+
+
+
+
+
+
+
+
+// fs.createReadStream(logExample)
+//     .pipe(csv.parse())
+//     .on('error', error => console.error(error))
+//     .on('data', row => writer.write(`ROW=${JSON.stringify(row)}`))
+//     .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
+
+
+
+
+
+
+
+
+
+
+
 
 // const title0 = "AWSLOGS"
 // const path0 = __dirname
