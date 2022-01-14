0ee5fa948ca6c963efc17ad0a05b0f96b65c976c
andrewsimmons <neon.duncan@gmail.com>
2022-01-14 13:58:37 -0600

    moved everything to app.js

--- app.js
+++ app.js
@@ -1,42 +1,71 @@
-'use strict';
-//Asynchonous
+const fs = require('fs')
+const path =require('path')
+var zlib = require('zlib')
+const readline = require('readline');
 
-const fs = require('fs');
-const path = require('path');
-
-// const readJson = file => {
-//     fs.readFile(file, 'utf8', (err, data) => {
-//         console.log(err)
-//         if (err) throw err;
-//         let student = JSON.parse(data);
-//         console.log(student);
-//         return student
-//     });
-// }
-const readJson = filePath => {
-    let rs = fs.createReadStream(filePath)
-    return rs
-    // rs.on('data', (dataChunk) => {
-    //     let student = JSON.parse(dataChunk)
-    //     console.log(student)
-    // })
+//returns a boolean for if file is a directory
+const isDir = (filePath) => {
+    const result = fs.statSync(filePath, (er) => {if (er) throw er})
+    return !result.isFile()
 }
 
-const writeJson = (data, file) => {
-    let jsonData = JSON.stringify(data, null, 2)
-    fs.writeFile(file, jsonData, {flag:'a+'}, (err) => {
-        if (err) throw err;
-        console.log('Data written to file');
+//if log files don't exists, it makes a new file and puts the headers
+const createLogsFile = () => {
+    const logFile = 'logs.csv'
+    fs.access(logFile, fs.constants.F_OK, (err) => {
+        if (err) {
+            const headers = "version,account-id,interface-id,srcaddr,dstaddr,srcport,dstport,protocol,packets,bytes,start,end,action,log-status"
+            fs.writeFileSync(logFile, headers)
+        } 
+      });
+      return logFile
+}
+
+const writeToCsv = filePath => {
+    const logFile = 'logs.csv'
+    const writeStream = fs.createWriteStream(logFile, {flags:'a'});
+    let lineReader = readline.createInterface({
+        input: fs.createReadStream(filePath).pipe(zlib.createGunzip()),
+    });
+    
+    lineReader.on('line', (line) => {
+        if (!line.includes('version')) {
+            writeStream.write(line.replaceAll(" ", ",") + "\n")
+        }
     });
 }
 
+//records only the zipped files
+const filterZipped = (filePath) => {
+    const filesNotLogged = []
+    if (path.extname(filePath) === '.gz') {
+        writeToCsv(filePath)
+    } else {
+        filesNotLogged.push(filePath)
+        if (filesNotLogged[0]) console.log(`There were ${filesNotLogged.length}: \n${filesNotLogged}`)
+    }
+}
 
-fs.readdirSync('students').forEach( fileName => {
-    let dirPath = path.join(__dirname, `/students/${fileName}`)
-    let student = readJson(dirPath)
-    student.on('data', (dataChunk) => {
-        writeJson(JSON.parse(dataChunk), 'data.json')
-    })
-})
-// const dirPath = path.join(__dirname, '/students/student.json')
-// readJson(dirPath)
+// using recursion, drills down to the files
+const drillDownToFiles = (rootPath, folder) => {
+    const dirTitle = fs.readdirSync(folder)[1]
+    const dirPath = path.join(rootPath, dirTitle)
+    const newFolderPath = `${folder}/${dirTitle}`
+
+    if (isDir(dirPath)) {
+        drillDownToFiles(dirPath, newFolderPath)
+   } else {
+        createLogsFile()
+        fs.readdirSync(folder).forEach(fileName => {
+            filterZipped(dirPath)
+        })
+        //I need to write a test that actually test whether that's true or not.
+        console.log("Files have been succesfully logged")
+    }
+}
+
+//Used to execute Looping through files:
+const awsLogs = "AWSLogs"
+const rootPath = path.join(__dirname, awsLogs)
+drillDownToFiles(rootPath, awsLogs)
+
