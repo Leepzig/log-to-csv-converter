11966c86c841b363c844a23a83297927bc1cc485
andrewsimmons <neon.duncan@gmail.com>
2022-01-14 13:43:03 -0600

    flowToCSV can take a folder and log all of the zipped files into a csv

--- NOTES.md
+++ NOTES.md
@@ -2,4 +2,11 @@ we need to read and write the files using streams
 when writing json, it needs to be formatted into an array, or something, depends
 now how to format json into a csv
 
-filter by .gz
+# Todo
+Think of edge cases to protect against
+ - filter by .gz
+ - skip first line
+
+
+headers = version,account-id,interface-id,srcaddr,dstaddr,srcport,dstport,protocol,packets,bytes,start,end,action,log-status
+const logExample = "AWSLogs/607487649814/vpcflowlogs/us-east-2/2022/01/13/05/607487649814_vpcflowlogs_us-east-2_fl-007edc593c7c0b891_20220113T0525Z_31dbb2ff.log.gz"
--- flowToCsv.js
+++ flowToCsv.js
@@ -1,16 +1,50 @@
-
 const fs = require('fs')
 const path =require('path')
-const { stringify } = require('querystring')
 var zlib = require('zlib')
-var gunzip = zlib.createGunzip()
 const readline = require('readline');
-
+const { Console } = require('console');
 
 //returns a boolean for if file is a directory
 const isDir = (filePath) => {
-    const result = fs.statSync(filePath, (er, stats) => {if (er) throw er})
+    const result = fs.statSync(filePath, (er) => {if (er) throw er})
     return !result.isFile()
+}
+
+//if log files don't exists, it makes a new file and puts the headers
+const createLogsFile = () => {
+    const logFile = 'logs.csv'
+    fs.access(logFile, fs.constants.F_OK, (err) => {
+        if (err) {
+            const headers = "version,account-id,interface-id,srcaddr,dstaddr,srcport,dstport,protocol,packets,bytes,start,end,action,log-status"
+            fs.writeFileSync(logFile, headers)
+        } 
+      });
+      return logFile
+}
+
+const writeToCsv = filePath => {
+    const logFile = 'logs.csv'
+    const writeStream = fs.createWriteStream(logFile, {flags:'a'});
+    let lineReader = readline.createInterface({
+        input: fs.createReadStream(filePath).pipe(zlib.createGunzip()),
+    });
+    
+    lineReader.on('line', (line) => {
+        if (!line.includes('version')) {
+            writeStream.write(line.replaceAll(" ", ",") + "\n")
+        }
+    });
+}
+
+//records only the zipped files
+const filterZipped = (filePath) => {
+    const filesNotLogged = []
+    if (path.extname(filePath) === '.gz') {
+        writeToCsv(filePath)
+    } else {
+        filesNotLogged.push(filePath)
+        if (filesNotLogged[0]) console.log(`There were ${filesNotLogged.length}: \n${filesNotLogged}`)
+    }
 }
 
 // using recursion, drills down to the files
@@ -18,86 +52,21 @@ const drillDownToFiles = (rootPath, folder) => {
     const dirTitle = fs.readdirSync(folder)[1]
     const dirPath = path.join(rootPath, dirTitle)
     const newFolderPath = `${folder}/${dirTitle}`
-    console.log("Folder: ", folder)
+
     if (isDir(dirPath)) {
         drillDownToFiles(dirPath, newFolderPath)
    } else {
-       console.log("was not folder", dirPath)
-    fs.readdirSync(folder).forEach(fileName => {
-        console.log("File:", fileName)
-        //unzip(dirpath/filename)
+        createLogsFile()
+        fs.readdirSync(folder).forEach(fileName => {
+            filterZipped(dirPath)
         })
+        //I need to write a test that actually test whether that's true or not.
+        console.log("Files have been succesfully logged")
     }
 }
+
 //Used to execute Looping through files:
-// const awsLogs = "AWSLogs"
-// const rootPath = path.join(__dirname, awsLogs)
-// drillDownToFiles(rootPath, awsLogs)
+const awsLogs = "AWSLogs"
+const rootPath = path.join(__dirname, awsLogs)
+drillDownToFiles(rootPath, awsLogs)
 
-const logExample = "AWSLogs/607487649814/vpcflowlogs/us-east-2/2022/01/13/05/607487649814_vpcflowlogs_us-east-2_fl-007edc593c7c0b891_20220113T0525Z_31dbb2ff.log.gz"
-
-const test = fs.readFileSync(logExample)
-
-// console.log(test.toString().replaceAll(" ", ","))
-const writeToCsv = filePath => {
-    let lineReader = readline.createInterface({
-        input: fs.createReadStream(filePath).pipe(zlib.createGunzip())
-    });
-    
-    lineReader.on('line', (line) => {
-        // fs.writeFileSync("logs.csv", line.replaceAll(" ", ","), {flags:'a'}, (err) => {
-            console.log(line.replaceAll(" ", ","))
-            // if (err) throw err
-        // });
-    });
-}
-
-writeToCsv(logExample)
-
-
-
-
-
-
-
-
-
-
-
-// fs.createReadStream(logExample)
-//     .pipe(csv.parse())
-//     .on('error', error => console.error(error))
-//     .on('data', row => writer.write(`ROW=${JSON.stringify(row)}`))
-//     .on('end', rowCount => console.log(`Parsed ${rowCount} rows`));
-
-
-
-
-
-
-
-
-
-
-
-
-// const title0 = "AWSLOGS"
-// const path0 = __dirname
-// const title1 = fs.readdirSync(title0)[1] // => 607487649814
-// const path1 = path.join(path0, title0, title1) // => full path to title1
-// console.log("Path1", path1)
-// // if path1 === dir
-// const title2 = fs.readdirSync(`AWSLogs/${title1}`)[1] // => vpclogs
-// console.log("Title2: ", title2)
-// const path2 = path.join(path1, title2) // => ?
-// console.log("Path2: ", path2)
-// //if path2 === dir
-// const title3 = fs.readdirSync(`AWSLogs/${title1}/${title2}`)[1] // => vpclogs
-// console.log("Title 3:", title3)
-// const path3 = path.join(path2, title3) // => ?
-// console.log("Path 3: ", path3)
-// //if path3 === dir
-// const title4 = fs.readdirSync(`AWSLogs/${title1}/${title2}/${title3}`)[1] // => vpclogs
-// console.log("Title 4:", title4)
-// const path4 = path.join(path3, title4) // => ?
-// console.log("Path 4: ", path4)
